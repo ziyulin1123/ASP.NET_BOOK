fb6eaa75b3cfe15f1c309782e4c99a5e39501309
ziyulin1123 <a0983984816@gmail.com>
2024-02-28 14:44:05 +0800

    20230228 版本紀錄

---
+++ TeaTimeDemo.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public
+    ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "果汁", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "茶", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "咖啡", DisplayOrder = 3 }
+                );
+        }
+    }
+}
---
+++ TeaTimeDemo.DataAccess/Migrations/20240227150719_AddCategoryToDbAndSeedTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TeaTimeDemo.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToDbAndSeedTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DisplayOrder = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "果汁" },
+                    { 2, 2, "茶" },
+                    { 3, 3, "咖啡" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
